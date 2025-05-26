50334e1b89c2b1b03b7449247074349d92049bab
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-26 14:37:23 -0300

    users-controller - data validation to register user - Zod.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import "express-async-errors";
 import express from "express";
 import cors from "cors";
 import { errorHandling } from "@/middlewares/error-handling";
 import { routes } from "@/routes";
+
 
 const app = express();
 app.use(express.json());
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
 import { z } from "zod";
+import { UserRole } from "@prisma/client";
 
 class UsersController {
   async create(request: Request, response: Response) {
-    
+    const bodySchema = z.object({
+      name: z
+        .string()
+        .trim()
+        .min(2, { message: "name must be at least 2 characters" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "invalid email" })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: "password must be at least 6 characters" }),
+      role: z
+        .enum([UserRole.employee, UserRole.manager])
+        .default(UserRole.employee),
+    });
+
+    const { name, email, password, role } = bodySchema.parse(request.body);
+
+    response.json({ name, email, password, role });
   }
 }
 
