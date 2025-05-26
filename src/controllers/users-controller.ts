461bb92036248558d7f60288634edf4ee885c518
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-26 10:47:02 -0300

    create route, controller, and index - for creating users

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+
+class UsersController {
+  async create(request: Request, response: Response) {
+    
+  }
+}
+
+export { UsersController };
