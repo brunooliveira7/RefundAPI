c0801c47fd1a3ae77ef9c4493358e2f7d71824c2
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-27 14:50:14 -0300

    request data validation - refunds-controller

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+
+const categoriesEnum = z.enum([
+  "food",
+  "other",
+  "services",
+  "transport",
+  "accomodation",
+]);
 
 class RefundsController {
   async create(request: Request, response: Response) {
-    response.status(200).json({ message: "Refunds created" });
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: "Name is required" }),
+      amount: z
+        .number()
+        .positive({ message: "Amount is required and must be positive" }),
+      category: categoriesEnum,
+      filename: z.string().min(20),
+    });
+
+    const { name, amount, category, filename } = bodySchema.parse(request.body);
+
+    response.status(200).json({ name, amount, category, filename });
   }
 }
 
