e2ee82684920f440760d5164b48e87ab694ab2b8
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-27 09:49:17 -0300

    create config folder - jwt. generate the token with the role, secret key and user data. then remove the password from the return.

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 import { compare } from "bcrypt";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
 
 class SessionsController {
   async create(request: Request, response: Response) {
@@ -27,7 +29,14 @@ class SessionsController {
       throw new AppError("Email or password incorrect", 401);
     }
 
-    response.json({ email, password });
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({ role: user.role }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+    const { password: _, ...userWithoutPassword } = user;
+
+    response.json({ token, user: userWithoutPassword });
   }
 }
 
