51d43b55e0f828d63fe8bd73947c2b653356dcd8
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-29 16:57:12 -0300

    feat: serves uploaded files via static route
    This commit configures a static route to serve uploaded files, making them accessible to clients.
    
    The changes include:
    
    - Adding a middleware to serve files from the `uploadConfig.UPLOADS_FOLDER` directory via the `/uploads` route.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import express from "express";
 import cors from "cors";
 import { errorHandling } from "@/middlewares/error-handling";
 import { routes } from "@/routes";
+import uploadConfig from "@/configs/upload";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
