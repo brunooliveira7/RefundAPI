fc685b10f0ddedc10429395a5aa30ab5fa70e6d5
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-05-29 09:14:39 -0300

    feat: validates file uploads and configures file size
    This commit introduces file validation and configures the maximum file size for uploads.
    
    The changes include:
    
    - Adding validation for filename, mimetype, and size in the `UploadsController`.
    - Defining a schema using Zod to validate the uploaded file.
    - Checking if the mimetype is in the accepted file types.
    - Checking if the file size exceeds the maximum allowed size.
    - Adding `MAX_SIZE` to upload config.
    - Returning an error if the file is invalid.
    - Updating comments in `src/configs/upload.ts` to clarify the purpose of temporary and uploads folders.
    - Adding temporary image files for testing.

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -2,12 +2,13 @@ import multer from "multer";
 import path from "node:path";
 import crypto from "node:crypto";
 
-//cria uma pasta para o arquivo ficar temporário - recebido do backend
+//cria uma pasta para o file ficar temporário - recebido do backend
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-///cria uma pasta uploads - arquivo final manipulado
+///cria uma pasta uploads para validação - file manipulado
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3; // 3MB
+const MAX_SIZE = 3; 
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE; // 3MB
 const ACCEPTED_FILE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
 
 //middleware
@@ -29,4 +30,5 @@ export default{
   MAX_FILE_SIZE,
   ACCEPTED_FILE_TYPES,
   MULTER,
+  MAX_SIZE,
 };
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,8 +1,36 @@
 import { Response, Request } from "express";
+import { z } from "zod";
+import uploadConfig from "@/configs/upload";
+import { AppError } from "@/utils/AppError";
 
 class UploadsController {
   async create(request: Request, response: Response) {
-    response.json({ message: "upload" });
+    try {
+      const fileSchema = z
+        .object({
+          filename: z.string().min(1, "filename is required"),
+          mimetype: z
+            .string()
+            .refine(
+              (type) => uploadConfig.ACCEPTED_FILE_TYPES.includes(type),
+              `Format file not accepted. Formats accepted: ${uploadConfig.ACCEPTED_FILE_TYPES}`
+            ),
+          size: z
+            .number()
+            .positive()
+            .refine(
+              (size) => size <= uploadConfig.MAX_FILE_SIZE,
+              `File size is too large. Max size: ${uploadConfig.MAX_SIZE}`
+            ),
+        })
+        .passthrough();
+
+        const {file} = fileSchema.parse(request.file);
+
+      response.json({message: "File sent successfully"});
+    } catch (error) {
+      throw new AppError("Erro ao fazer upload de arquivo");
+    }
   }
 }
 
